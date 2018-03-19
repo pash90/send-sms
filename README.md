ee24b100faafb502020e2871c0a883e45b8dda8e
Parth Mehta <parth@bluechilli.com>
2018-03-19 22:37:02 +1100

    validation for phone and message fields

---
+++ README.md
@@ -0,0 +1,6 @@
+This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app). Please read the instructions there for how-tos and troubleshooting.
+
+## Dependencies
+[axios](https://www.npmjs.com/package/axios), [get-urls](https://www.npmjs.com/package/get-urls), [bitly](https://www.npmjs.com/package/bitly), [node-sass-chokidar](https://www.npmjs.com/package/node-sass-chokidar) & [npm-run-all](https://www.npmjs.com/package/npm-run-all)(_for SASS support_)
+
+## Get Started
--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -2,20 +2,67 @@
 import * as React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
+/** Interfaces */
+interface FormValues {
+  phone?: string,
+  message?: string
+}
+
 /** Styles */
 import './index.css';
 
-/** Form Component */
-const SMSForm = reduxForm({
-  form: 'sms'
-})(props => {
+/** Form */
+const renderField = (field: any) => {
+  const { input, meta: { error }, placeholder, type } = field;
+  const { value } = input;
 
-  const { handleSubmit } = props;
+  if (field.type === "textarea") {
+    return (
+      <div className={`input-area multi ${error ? 'error' : ''}`}>
+        <textarea {...input} placeholder={placeholder} required />
+        {value ? (
+          <p className="count">{459 - value.length}</p>
+        ) : (
+            <p className="count">459</p>
+          )}
+      </div>
+    )
+  }
+
+  return (
+    <div className={`input-area single ${error ? 'error' : ''}`}>
+      <input {...input} type={type} placeholder={placeholder} required />
+      {error && <p className="count">{error}</p>}
+    </div>
+  )
+}
+
+const SMSForm = reduxForm<FormValues>({
+  form: 'sms',
+  validate: values => {
+    const phoneNo = /^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,3})|(\(?\d{2,3}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$/;
+
+    const errors = {
+      message: values.message && values.message.length > 459 ? "To many characters" : undefined,
+      phone: values.phone && !values.phone.match(phoneNo) ? "Invalid Number" : undefined
+    }
+
+    return errors;
+  }
+})(props => {
+  const { handleSubmit, valid } = props;
 
   return (
     <form onSubmit={handleSubmit}>
-      <Field name="phone" component="input" required placeholder="Mobile Number" />
-      <Field name="message" component="textarea" required validate={value => value.length <= 459} placeholder="Message" />
+      <Field name="phone" placeholder="Mobile Number" type="tel" component={renderField} />
+      <Field name="message" component={renderField} type="textarea" placeholder="Message" />
+
+      {valid ? (
+        <button type="submit" className="submit-button">Send</button>
+      ) : (
+          <button disabled className="disabled-button">Send</button>
+        )}
+
     </form>
   )
 })
@@ -24,7 +71,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <SMSForm onSubmit={() => { }} />
+        <SMSForm onSubmit={values => console.log(values)} />
       </div>
     );
   }
